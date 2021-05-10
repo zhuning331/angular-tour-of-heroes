62ea8ff25f28c2717dfe550d82db124fba601466
Jhu-Ning Chang <zhuning331@gmail.com>
2021-05-10 18:22:31 +0800

    finish template-driven form

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,11 +1,13 @@
 export interface IHero {
   id?: number;
   name?: string;
+  email?: string;
 }
 
 export class Hero {
   constructor(
     public id?: number,
-    public name?: string
+    public name?: string,
+    public email?: string
   ) {}
 }
